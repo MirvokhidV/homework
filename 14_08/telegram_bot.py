2fc618a0465746a4ebb9af5d06a372e5818933fd
MirvokhidV <mirvohidvosiljonov@gmail.com>
2023-07-15 21:00:10 +0500

    telegram-bot-14.08 second commit

--- 14_08/telegram_bot.py
+++ 14_08/telegram_bot.py
@@ -11,13 +11,9 @@ for result in req['result']:
             requ = requests.get(
                 f"https://api.telegram.org/bot5724766211:AAFzpwZlPhmtr2px_weZ-Kr5dsXemfkJGL4/sendMessage?chat_id={str(result['message']['chat']['id'])}&text=Salom"
             ).json()
-            # print(result['message']['chat']['id'])
-            # print('ho')
         if result['message']['text'].lower() != 'salom':
             r = requests.get(
                 f"https://api.telegram.org/bot5724766211:AAFzpwZlPhmtr2px_weZ-Kr5dsXemfkJGL4/sendMessage?chat_id={str(result['message']['chat']['id'])}&text=<strong>sizning xabarlaringiz:</strong>\n\n{result['message']['text']}&parse_mode=HTML"
             ).json()
     except:
         pass
-    # if result['message']['text'].lower() == 'salom':
-    #     print('Cho tam mujik!')
